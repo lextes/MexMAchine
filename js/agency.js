c3f7de798bbad5f3f68aa220930d87cdf90a5cdc
lextes <forza1708@gmail.com>
2018-11-12 22:55:54 -0600

    Add files via upload

---
+++ js/agency.js
@@ -0,0 +1,50 @@
+(function($) {
+  "use strict"; // Comienza a estrechar
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Se cierra el responsive menu cuando se da click
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activa mediante scollspy las clases para los items
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse ahora la página no está al top
+  navbarCollapse();
+  // Collapse el navbar cuando la página está en movimiento
+  $(window).scroll(navbarCollapse);
+
+  // Esconde el navbar cuando se la da click
+  $('.portfolio-modal').on('show.bs.modal', function(e) {
+    $('.navbar').addClass('d-none');
+  })
+  $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $('.navbar').removeClass('d-none');
+  })
+
+})(jQuery); 
